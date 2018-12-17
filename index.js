77ecfcb9fbef3fa04d732e79006e4548af6d5c59
unknown <benjpas@MININT-OABU3P8.europe.corp.microsoft.com>
2018-12-17 15:23:58 +0100

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const vm = require("vm");
+const fs = require("fs");
+
+const CACHE_FILE = 'cache.code';
+
+let options;
+if (fs.existsSync(CACHE_FILE)) {
+    options = { cachedData: fs.readFileSync(CACHE_FILE) };
+} else {
+    options = { produceCachedData: true, displayErrors: true };
+}
+
+const now = Date.now();
+const script = new vm.Script(fs.readFileSync('typescript.js').toString('utf8'), options);
+console.log((Date.now() - now) + "ms");
+
+if (script.cachedDataProduced) {
+    fs.writeFileSync(CACHE_FILE, script.cachedData);
+    console.log("Did produce cached data");
+} else {
+    if (script.cachedDataRejected) {
+        console.log("Did not produce cached data. IT WAS REJECTED!");
+    } else {
+        console.log("Did not produce cached data. Loaded from cache!");
+    }
+}
+
